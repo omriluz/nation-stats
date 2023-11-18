b47396018e522a4622db9946130a2f496c49a751
Omri <omriluz1994@gmail.com>
2023-11-19 00:43:34 +0200

    error in vercel url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
-const site = VERCEL_URL || 'http://localhost:3000'
+const site = `https://${VERCEL_URL}.com` || 'http://localhost:3000'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
