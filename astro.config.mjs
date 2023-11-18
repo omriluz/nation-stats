420407677ebdfb593748932ad89c7a93f33945ef
Omri <omriluz1994@gmail.com>
2023-11-19 00:41:17 +0200

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+const site = VERCEL_URL || 'http://localhost:3000'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()],
+  site
 });
